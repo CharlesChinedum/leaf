fb2673a08f4c014a1fb4b19338eca11421119655
CharlesChinedum <charleschinedum2@gmail.com>
2023-09-01 16:20:31 +0100

    started working on bookmark page

--- app/(dashboard)/(routes)/bookmark/page.tsx
+++ app/(dashboard)/(routes)/bookmark/page.tsx
@@ -1,7 +1,19 @@
+import Heading from "@/components/Heading";
+import { BookmarkPlusIcon } from "lucide-react";
 import React from "react";
 
 const BookmarkPage = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full">
+      <Heading
+        title="Bookmark your favorite links"
+        description="Save your favorite links and access them from anywhere."
+        icon={BookmarkPlusIcon}
+        iconColor="text-violet-500"
+        bgColor="bg-violet-500/10"
+      />
+    </div>
+  );
 };
 
 export default BookmarkPage;
