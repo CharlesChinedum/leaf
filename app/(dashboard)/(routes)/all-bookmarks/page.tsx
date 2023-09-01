39080afc146e7442ae36832444acb75ce090a719
CharlesChinedum <charleschinedum2@gmail.com>
2023-09-01 16:29:44 +0100

    update: added Heading component to all bookmarks page

--- app/(dashboard)/(routes)/all-bookmarks/page.tsx
+++ app/(dashboard)/(routes)/all-bookmarks/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { ScrollTextIcon } from "lucide-react";
+
+import Heading from "@/components/Heading";
 
 const AllBookmarksPage = () => {
-  return <div>All bookmarks</div>;
+  return (
+    <div className="w-full">
+      <Heading
+        title="All Bookmarks"
+        description="All your bookmarks in one place."
+        icon={ScrollTextIcon}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+    </div>
+  );
 };
 
 export default AllBookmarksPage;
